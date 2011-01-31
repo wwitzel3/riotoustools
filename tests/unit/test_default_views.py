e9579a9f90bba81f74e94dee6ee60f3f7ef12106
Wayne Witzel III <wwitzel3@gmail.com>
2011-01-31 16:37:30 -0500

    refactored some tests and mocked out ZZ hg push

--- tests/unit/test_default_views.py
+++ tests/unit/test_default_views.py
@@ -7,7 +7,7 @@ class DefaultViewTest(TestCase):
     def setUp(self):
         self.user = models.user.User('demo@example.com', 'password', 'name')
         self.user.id = 1
-              
+        
     def testIndexView(self):
         response = default.index(self.request)
         self.assertEquals(response, dict())
@@ -30,29 +30,64 @@ class DefaultViewTest(TestCase):
         self.assertTrue(response.has_key('login_form'))
         self.assertTrue(response.has_key('signup_form'))
         
-    def testPresentLoginViewNext(self):
-        self.request.params = {'next':'dayzero'}
-        response = default.present_login(self.request)
-        self.assertEquals(self.request.next, 'http://example.com/dayzero')
-        
-    def testPresentLoginViewLogin(self):
-        mock_session = Mock('session')
-        mock_result_proxy = Mock('result_proxy')
-        mock_result_proxy.one.mock_returns = self.user
-        
-        mock_query = Mock('query')
-        mock_query.filter_by = Mock('filter_by', returns=mock_result_proxy)
-        mock_session.query = Mock('query', returns=mock_query)
-        models.DBSession.mock_returns = mock_session
-           
+    def testPresentLoginInvalidLogin(self):           
         self.request.params = {'form.login':True}
         response = default.present_login(self.request)
+
         self.assertTrue(response.has_key('login_form'))
+        
+        login_form = response.get('login_form')
+        self.assertTrue('for: email' in login_form)
+        self.assertTrue('for: password' in login_form)
+        
+    def testPresentLoginValidLogin(self):
+        self.mock_session = Mock('session')
+        self.mock_result_proxy = Mock('result_proxy')
+        self.mock_result_proxy.one.mock_returns = self.user
+        
+        self.mock_query = Mock('query')
+        self.mock_query.filter_by = Mock('filter_by', returns=self.mock_result_proxy)
+        self.mock_session.query = Mock('query', returns=self.mock_query)
+        models.DBSession.mock_returns = self.mock_session
         
         self.request.params = {
             'form.login':True,
             'email':'demo@example.com',
-            'password':'password'
+            'password':'password',
+            'next':'/dayzero'
         }
-        respone = default.present_login(self.request)
+        
+        response = default.present_login(self.request)
+        self.assertEquals(response.location, 'http://example.com/dayzero/')
+        
+    def testPresentLoginValidSignup(self):
+        self.mock_session = Mock('session')
+        self.mock_result_proxy = Mock('result_proxy')
+        
+        from sqlalchemy.orm.exc import NoResultFound
+        def one():
+            raise NoResultFound
+        self.mock_result_proxy.one = one
+        
+        self.mock_query = Mock('query')
+        self.mock_query.filter_by = Mock('filter_by', returns=self.mock_result_proxy)
+        self.mock_session.query = Mock('query', returns=self.mock_query)
+        models.DBSession.mock_returns = self.mock_session
+        
+        self.request.params = {
+            'form.create': True,
+            'email':'test@example.com',
+            'password':'password',
+            'password_verify':'password',
+            'name':'Demo',
+        }
+        self.mock_result_proxy.one.mock_returns = None
+        response = default.present_login(self.request)
+        print response
+        assert False
+        
+    def testLogout(self):
+        response = default.logout(self.request)
         self.assertEquals(response.location, 'http://example.com/')
+        
+        
